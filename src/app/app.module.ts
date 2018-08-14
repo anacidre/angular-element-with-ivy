449b3c315b950b98388410d4c6ae4325f564146a
Ana Cidre <ana@anacidre.com>
2018-08-14 11:06:44 +0200

    Update module for Angular Element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
-import { AppComponent } from './app.component';
+import { CardComponent } from './card-component/card-component.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [CardComponent],
+  imports: [BrowserModule],
+  entryComponents: [CardComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const bbCard = createCustomElement(CardComponent, {
+      injector: this.injector,
+    });
+    console.log(bbCard);
+    customElements.define('bb-card', bbCard);
+  }
+  ngDoBootstrap() {}
+}
